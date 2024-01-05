5f40794f8c37bebd2c2dd5937d4bd44c5fff290f
mail-aizhan <65739091+mail-aizhan@users.noreply.github.com>
2024-01-05 18:26:42 +0600

    firebase connected

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const inputFieldEl= document.getElementById("input-field")
-const addButtonEl =document.getElementById("add-button")
+import { initializeApp } from "firebase/app";
+import { getDatabase,ref, push } from "firebase/database";
 
-addButtonEl.addEventListener("click",function (){
-    let inputValue=inputFieldEl.value
-    console.log(inputFieldEl.value)
+const appSettings = {
+    getDatabaseURL: "https://addtochart-fddab-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    console.log(inputValue)
 })
